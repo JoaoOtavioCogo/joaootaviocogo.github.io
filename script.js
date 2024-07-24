ff5c24e7bd96fc9500e6e07b50f427911eba12a8
JoaoOtavioCogo <joao.cogo@unesp.br>
2024-07-24 16:21:11 -0300

    Upload files

--- script.js
+++ script.js
@@ -2,31 +2,30 @@ document.getElementById('addPlayerForm').addEventListener('submit', function(eve
     event.preventDefault();
 
     const name = document.getElementById('name').value;
-    const level = parseInt(document.getElementById('level').value);
     const goals = parseInt(document.getElementById('goals').value);
     const assists = parseInt(document.getElementById('assists').value);
     const wins = parseInt(document.getElementById('wins').value);
     const losses = parseInt(document.getElementById('losses').value);
 
-    fetch('/players', {
+    const level = wins * 20 - losses * 20 + 250;
+
+    fetch('http://localhost:3000/players', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
         },
-        body: JSON.stringify({ name, level, goals, assists, wins, losses }),
+        body: JSON.stringify({ id: Date.now().toString(), name, level, goals, assists, wins, losses }),
     })
     .then(response => response.text())
     .then(data => {
         console.log('Sucesso:', data);
         alert('Jogador adicionado com sucesso');
-        // Limpar os campos do formulário após adicionar o jogador
         document.getElementById('name').value = '';
-        document.getElementById('level').value = '';
         document.getElementById('goals').value = '';
         document.getElementById('assists').value = '';
         document.getElementById('wins').value = '';
         document.getElementById('losses').value = '';
-        fetchAndDisplayPlayers(); // Atualizar a lista de jogadores
+        fetchAndDisplayPlayers();
     })
     .catch((error) => {
         console.error('Erro:', error);
@@ -35,13 +34,21 @@ document.getElementById('addPlayerForm').addEventListener('submit', function(eve
 });
 
 function fetchAndDisplayPlayers() {
-    fetch('/players')
+    fetch('http://localhost:3000/players')
         .then(response => response.json())
         .then(players => {
-            const playersList = document.getElementById('playersList');
-            playersList.innerHTML = '';
+            const playersList1Star = document.getElementById('playersList-1-star');
+            const playersList2Star = document.getElementById('playersList-2-star');
+            const playersList3Star = document.getElementById('playersList-3-star');
+            const playersList4Star = document.getElementById('playersList-4-star');
+            const playersList5Star = document.getElementById('playersList-5-star');
 
-            // Classificar jogadores pelo nível em ordem decrescente
+            playersList1Star.innerHTML = '';
+            playersList2Star.innerHTML = '';
+            playersList3Star.innerHTML = '';
+            playersList4Star.innerHTML = '';
+            playersList5Star.innerHTML = '';
+
             players.sort((a, b) => b.level - a.level);
 
             players.forEach(player => {
@@ -50,56 +57,74 @@ function fetchAndDisplayPlayers() {
 
                 const playerInfo = document.createElement('div');
                 playerInfo.className = 'player-info';
-                playerInfo.textContent = `${player.name} - Nível: ${player.level} - Gols: ${player.goals} - Assistências: ${player.assists} - Vitórias: ${player.wins} - Derrotas: ${player.losses}`;
+                playerInfo.textContent = `${player.name} - Gols: ${player.goals} - Assistências: ${player.assists} - Vitórias: ${player.wins} - Derrotas: ${player.losses}`;
 
                 const stars = document.createElement('span');
                 stars.className = 'stars';
-                stars.textContent = '★'.repeat(player.level);
+                stars.textContent = '★'.repeat(Math.floor(player.level / 100) + 1);  // Lógica atualizada para estrelas
 
                 const progressBar = document.createElement('div');
                 progressBar.className = 'progress-bar';
-
                 const progress = document.createElement('div');
                 progress.className = 'progress';
-                progress.style.width = `${(player.level / 5) * 100}%`;
-
+                progress.style.width = `${(player.level % 100)}%`;
                 progressBar.appendChild(progress);
+
                 playerItem.appendChild(playerInfo);
                 playerItem.appendChild(stars);
                 playerItem.appendChild(progressBar);
-                playersList.appendChild(playerItem);
+
+                if (player.level >= 0 && player.level < 100) {
+                    playersList1Star.appendChild(playerItem);
+                } else if (player.level >= 100 && player.level < 200) {
+                    playersList2Star.appendChild(playerItem);
+                } else if (player.level >= 200 && player.level < 300) {
+                    playersList3Star.appendChild(playerItem);
+                } else if (player.level >= 300 && player.level < 400) {
+                    playersList4Star.appendChild(playerItem);
+                } else if (player.level >= 400) {
+                    playersList5Star.appendChild(playerItem);
+                }
             });
         })
-        .catch((error) => {
-            console.error('Erro ao buscar jogadores:', error);
-        });
+        .catch(error => console.error('Erro ao buscar jogadores:', error));
 }
 
-// Função para abrir uma aba e fechar as outras
 function openTab(evt, tabName) {
-    var i, tabcontent, tablinks;
-
-    // Esconder todas as abas
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = "none";
+    const tabcontent = document.getElementsByClassName('tabcontent');
+    for (let i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = 'none';
     }
 
-    // Remover a classe "active" de todos os botões de aba
-    tablinks = document.getElementsByClassName("tablink");
-    for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    const tablinks = document.getElementsByClassName('tablink');
+    for (let i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(' active', '');
     }
 
-    // Mostrar a aba atual e adicionar uma classe "active" ao botão de aba
-    document.getElementById(tabName).style.display = "block";
-    evt.currentTarget.className += " active";
+    document.getElementById(tabName).style.display = 'block';
+    evt.currentTarget.className += ' active';
+
+    if (tabName === 'rankingTab') {
+        fetchAndDisplayPlayers();
+    }
 }
 
-// Chamar a função para exibir jogadores ao carregar a página
-fetchAndDisplayPlayers();
+function openSubTab(evt, subTabName) {
+    const subtabcontent = document.getElementsByClassName('subtabcontent');
+    for (let i = 0; i < subtabcontent.length; i++) {
+        subtabcontent[i].style.display = 'none';
+    }
 
-// Abrir a primeira aba por padrão
-document.addEventListener('DOMContentLoaded', function() {
-    document.querySelector('.tablink').click();
+    const subtablinks = document.getElementsByClassName('tablink');
+    for (let i = 0; i < subtablinks.length; i++) {
+        subtablinks[i].className = subtablinks[i].className.replace(' active', '');
+    }
+
+    document.getElementById(subTabName).style.display = 'block';
+    evt.currentTarget.className += ' active';
+}
+
+// Carrega a primeira aba por padrão
+document.addEventListener('DOMContentLoaded', () => {
+    document.getElementsByClassName('tablink')[0].click();
 });
--- server/server.js
+++ server/server.js
@@ -7,9 +7,14 @@ const app = express();
 const PORT = 3000;
 
 app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname, '../Public')));
+app.use(express.static(path.join(__dirname, '../')));
 
 const playersFilePath = path.join(__dirname, '../players.json');
+
+// Servir o index.html
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../index.html'));
+});
 
 // Endpoint para obter todos os jogadores
 app.get('/players', (req, res) => {
@@ -50,18 +55,56 @@ app.post('/players', (req, res) => {
     });
 });
 
-app.listen(PORT, () => {
-    console.log(`Servidor rodando em http://localhost:${PORT}`);
-});
+// Endpoint para editar um jogador
+app.put('/players/:id', (req, res) => {
+    const playerId = req.params.id;
+    const updatedPlayer = req.body;
 
-app.get('/players', (req, res) => {
     fs.readFile(playersFilePath, 'utf8', (err, data) => {
         if (err) {
             console.error('Erro ao ler o arquivo de jogadores:', err);
             res.status(500).send('Erro ao ler o arquivo de jogadores');
             return;
         }
-        const players = JSON.parse(data);
-        res.json(players);
+        let players = JSON.parse(data);
+        players = players.map(player => player.id === playerId ? updatedPlayer : player);
+
+        fs.writeFile(playersFilePath, JSON.stringify(players, null, 2), 'utf8', (err) => {
+            if (err) {
+                console.error('Erro ao salvar o jogador editado:', err);
+                res.status(500).send('Erro ao salvar o jogador editado');
+                return;
+            }
+            res.status(200).send('Jogador editado com sucesso');
+        });
     });
 });
+
+// Endpoint para excluir um jogador
+app.delete('/players/:id', (req, res) => {
+    const playerId = req.params.id;
+
+    fs.readFile(playersFilePath, 'utf8', (err, data) => {
+        if (err) {
+            console.error('Erro ao ler o arquivo de jogadores:', err);
+            res.status(500).send('Erro ao ler o arquivo de jogadores');
+            return;
+        }
+        let players = JSON.parse(data);
+        players = players.filter(player => player.id !== playerId);
+
+        fs.writeFile(playersFilePath, JSON.stringify(players, null, 2), 'utf8', (err) => {
+            if (err) {
+                console.error('Erro ao excluir o jogador:', err);
+                res.status(500).send('Erro ao excluir o jogador');
+                return;
+            }
+            res.status(200).send('Jogador excluído com sucesso');
+        });
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`Servidor rodando em http://localhost:${PORT}`);
+});
+
